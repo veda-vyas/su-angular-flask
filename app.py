1267a31cc13005309d0d99f651116cfc0c9aba3a
Veda Vyas <vedavyas.yeleswarapu@gmail.com>
2018-02-27 05:26:13 +0530

    Completed signup handler

--- app.py
+++ app.py
@@ -22,9 +22,56 @@ def print_name(name):
         )
         return {"error": errors}
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    results = []
+    if request.method == "POST":
+        try:
+            name = request.form["name"]
+            password = request.form["password"]
+            email = request.form["email"]
+            user = users(
+                name=name,
+                password=password,
+                email=email
+            )
+            db.session.add(user)
+            db.session.commit()
+            results.append(
+                "Successfully Registered."
+            )
+        except Exception as e:
+            print e
+            errors.append("Unable to add user into database.")            
+        
+    return render_template('index.html', errors=errors, results=results)
+
+@app.route('/register',methods=['POST'])
+def register():
+    errors = []
+    results = []
+    if request.method == "POST":
+        # get url that the user has entered
+        try:
+            name = request.form["name"]
+            password = request.form["password"]
+            email = request.form["email"]
+            user = users(
+                name=name,
+                password=password,
+                email=email
+            )
+            db.session.add(user)
+            db.session.commit()
+            results.append(
+                "Successfully Registered."
+            )
+        except Exception as e:
+            print e
+            errors.append("Unable to add user into database.")
+            
+        return redirect(url_for('index', errors=errors, results=results))
 
 @app.route('/<name>')
 def hello_name(name):
