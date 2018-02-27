1c232cc5f8aad83f78fd8f1fa82999b0d7802f94
Veda Vyas <vedavyas.yeleswarapu@gmail.com>
2018-02-27 07:09:45 +0530

    Added initial angular deps

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from rq import Queue
 from rq.job import Job
 from worker import conn
 from flask.ext.sqlalchemy import SQLAlchemy
+import json
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
@@ -12,73 +13,39 @@ from models import users
 
 q = Queue(connection=conn)
 
-def print_name(name):
+def register(name, email, password):
     errors = []
+    results = []
     try:
-      	return name
-    except:
-        errors.append(
-            "Unable to get Name. Please make sure it's valid and try again."
+        user = users(
+            name=name,
+            password=password,
+            email=email
         )
+        db.session.add(user)
+        db.session.commit()
+        results.append("Successfully Registered")
+        return {"result": results}
+    except:
+        errors.append("Unable to add user into database.")
         return {"error": errors}
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    errors = []
-    results = []
-    if request.method == "POST":
-        try:
-            name = request.form["name"]
-            password = request.form["password"]
-            email = request.form["email"]
-            user = users(
-                name=name,
-                password=password,
-                email=email
-            )
-            db.session.add(user)
-            db.session.commit()
-            results.append(
-                "Successfully Registered."
-            )
-        except Exception as e:
-            print e
-            errors.append("Unable to add user into database.")            
-        
-    return render_template('index.html', errors=errors, results=results)
+def index():            
+    return render_template('index.html')
 
-@app.route('/register',methods=['POST'])
-def register():
-    errors = []
-    results = []
-    if request.method == "POST":
-        # get url that the user has entered
-        try:
-            name = request.form["name"]
-            password = request.form["password"]
-            email = request.form["email"]
-            user = users(
-                name=name,
-                password=password,
-                email=email
-            )
-            db.session.add(user)
-            db.session.commit()
-            results.append(
-                "Successfully Registered."
-            )
-        except Exception as e:
-            print e
-            errors.append("Unable to add user into database.")
-            
-        return redirect(url_for('index', errors=errors, results=results))
+@app.route('/signup', methods=['POST'])
+def signup():
+    # get url
+    data = json.loads(request.data.decode())
+    name = data["name"]
+    email = data["email"]
+    password = data["password"]
 
-@app.route('/<name>')
-def hello_name(name):
-    results = {}
-    job = q.enqueue_call(func="app.print_name", args=(name,), result_ttl=5000)
-    jobid = job.get_id()
-    return render_template('index.html', results=jobid)
+    job = q.enqueue_call(
+        func="app.register", args=(name,email,password), result_ttl=5000
+    )
+    return job.get_id()
 
 @app.route("/results/<job_key>", methods=['GET'])
 def get_results(job_key):
@@ -86,9 +53,9 @@ def get_results(job_key):
     job = Job.fetch(job_key, connection=conn)
 
     if job.is_finished:
-        return str(job.result), 200
+        return jsonify(job.result)
     else:
-        return "Still Proceesing.", 202
+        return "Still Processing", 202
 
 if __name__ == '__main__':
     app.run()
--- static/main.js
+++ static/main.js
@@ -1,13 +1,51 @@
 (function () {
   'use strict';
 
-  angular.module('IndexPage', [])
+  angular.module('WordcountApp', [])
 
-  .controller('IndexController', ['$scope', '$log',
-    function($scope, $log) {
+  .controller('WordcountController', ['$scope', '$log', '$http', '$timeout',
+    function($scope, $log, $http, $timeout) {
       $scope.getResults = function() {
         $log.log("test");
+
+        var name = $scope.name;
+        var email = $scope.email;
+        var password = $scope.password;
+
+	    // fire the API request
+	    $http.post('/signup', {"name": name,"email": email,"password": password}).
+	      success(function(results) {
+	        $log.log(results);
+	        getStatus(results);
+	      }).
+	      error(function(error) {
+	        $log.log(error);
+	      });
       };
+
+    function getStatus(jobID) {
+      var timeout = "";
+
+      var poller = function() {
+        // fire another request
+        $http.get('/results/'+jobID).
+          success(function(data, status, headers, config) {
+            if(status === 202) {
+              $log.log(data, status);
+            } else if (status === 200){
+              $log.log(data);
+              $scope.result = data;
+              $timeout.cancel(timeout);
+              return false;
+            }
+            // continue to call the poller() function every 2 seconds
+            // until the timeout is cancelled
+            timeout = $timeout(poller, 2000);
+          });
+      };
+      poller();
+    }
+
     }
   ]);
 
